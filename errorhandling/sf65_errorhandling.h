c6a76b756d1a473ef8fe82ae2416e71608718522
mc78 <mc78@outlook.de>
2018-05-02 07:51:03 +0200

    Added style sheet for sf65 homepage

--- errorhandling/sf65_errorhandling.h
+++ errorhandling/sf65_errorhandling.h
@@ -1,8 +1,21 @@
 #ifndef __SF65ERRHANDL_H__
 #define __SF65ERRHANDL_H__
 
+/*
+ * Define a list of errors which may occur during parsing.
+ * The idea is of ERROR is that some functions may indicate errornous condition but
+ * if caller needs more info he has to call sf65_GetError function or such.
+ */
+#define SF65_ERRLIST ER(NOERR), ER(ERROR), ER(NULLPTR), ER(INVALIDARGERR)
+
+#define ER(x) SF65_##x
+typedef enum {
+    SF65_ERRLIST
+} sf65ErrCode_t;
+#undef ER
+
 typedef struct sf65ErrorDesc_tag {
-    sf65_ErrCode_t errcode;
+    sf65ErrCode_t errcode;
     char msg[80];
 } sf65ErrorDesc_t;
 
@@ -15,5 +28,9 @@ void sf65_pError ( char *format, ... );
  * Prints a formatted string to stderr but take va_list object as second arg
  */
 void sf65_vpError ( char *format, va_list va );
+
+sf65ErrCode_t sf65_SetError ( sf65ErrCode_t code, const char* const msg );
+sf65ErrCode_t sf65_GetLastError ( void );
+bool sf65_ErrorOccured ( void );
 
 #endif
